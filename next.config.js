679024ea50894de87f2119a41cbdd98c4411d149
ahmed <ahmed@debian.ahmed>
2022-08-21 01:16:31 +0200

    ain

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+ 
 }
 
 module.exports = nextConfig
 module.exports = {
+   redirects:async () =>{
+    return [
+      {
+        source: '/product1',
+        destination: '/404',
+        permanent: true,
+      },
+    ];
+  },
   images:{
     domains: ["i.im.ge"]
   }
